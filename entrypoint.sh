8466259fa87b2f4eb98d44f5e64fa4387ce87e26
GoodbyeNJN <cc@fuckwall.cc>
2024-10-29 00:11:51 +0800

    fix: change aria2 working directory to `/opt/aria2`

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 chown -R ${PUID}:${PGID} /opt/alist/
+chown -R ${PUID}:${PGID} /opt/aria2/
 
 umask ${UMASK}
 
 exec su-exec ${PUID}:${PGID} nohup aria2c \
   --enable-rpc \
   --rpc-allow-origin-all \
-  --conf-path=/root/.aria2/aria2.conf \
-  >/dev/null 2>&1 &
+  --conf-path=/opt/aria2/data/aria2.conf \
+  >/opt/aria2/data/log/log.log 2>&1 &
 
 exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 apk add --no-cache aria2 curl
 
-mkdir -p /root/.aria2
-cd /root/.aria2
+mkdir -p /opt/aria2/data/log
+cd /opt/aria2/data
 wget https://github.com/P3TERX/aria2.conf/archive/refs/heads/master.tar.gz
 tar -zxvf master.tar.gz --strip-components=1
 rm -rf master.tar.gz
 sed -i 's|rpc-secret|#rpc-secret|g' ./aria2.conf
-touch /root/.aria2/aria2.session
+sed -i 's|/root/.aria2/aria2.session|/opt/aria2/data/aria2.session|g' ./aria2.conf
+touch /opt/aria2/data/aria2.session
 ./tracker.sh

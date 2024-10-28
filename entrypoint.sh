616b6c72670bd0d1b14ba09e8ca4cd0e1610c9ae
GoodbyeNJN <cc@fuckwall.cc>
2024-10-29 00:29:30 +0800

    fix: change aria2 working directory to `/opt/aria2`

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ exec su-exec ${PUID}:${PGID} nohup aria2c \
   --enable-rpc \
   --rpc-allow-origin-all \
   --conf-path=/opt/aria2/data/aria2.conf \
-  >/opt/aria2/data/log/log.log 2>&1 &
+  >/opt/aria2/data/aria2.log 2>&1 &
 
 exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
